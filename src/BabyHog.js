8b683053729a2c62b13cf5131963e29f820a0d81
Don Mallory <dmm333@gmail.com>
2020-06-24 12:46:56 -0400

    6 24 solution

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
-import normalBaby from './assets/unadulterated-hoglette.png'
+// import normalBaby from './assets/unadulterated-hoglette.png'
 import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
@@ -12,6 +12,9 @@ export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+              weight: 35
+    }
   }
 
   changeWeight = (e) => {
@@ -22,23 +25,34 @@ export default class BabyHog extends Component {
     })
   }
 
+  pickBabyImage() {
+    switch (this.props.eyeColor) {
+      case "blue":
+        return BlueBaby
+      case "sun":
+        return SunBaby
+      case "glowing":
+        return GlowingBaby
+    }
+  }
+
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>{this.props.attributes.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h3>Hobby: {this.props.attributes.hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
           
-        <Button name="+">
+        <Button onClick={this.changeWeight} name="+">
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button onClick={this.changeWeight} name="-">
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={this.pickBabyImage()} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
         
       </li>
